457af030a694ae1ef65e570a56b128d901e77cb9
Luke Arntz <luke@blue42.net>
2023-02-20 12:47:58 -0500

    write results to mongo, add region var

--- cmd/controller/controller.go
+++ cmd/controller/controller.go
@@ -4,23 +4,24 @@ package controller
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
+	"github.com/larntz/status/internal/application"
 	"github.com/larntz/status/internal/data"
+	log "github.com/sirupsen/logrus"
 )
 
 // StartController runs the controller
-func StartController() {
+func StartController(app *application.State) {
 
 	// handle route using handler function
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		region := "us-east-1"
-		checks := data.GetChecks(region)
-		log.Printf("Loaded %d checks for region '%s'", len(checks.StatusChecks), region)
+		checks := data.GetChecks(app.DbClient, region)
+		log.Infof("Loaded %d checks for region '%s'", len(checks.StatusChecks), region)
 		response, err := json.Marshal(checks)
 		if err != nil {
-			fmt.Println("error marshalling check")
+			log.Error("error marshalling check")
 		}
 		fmt.Fprintf(w, string(response))
 	})
--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -2,15 +2,16 @@
 package worker
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
+	"context"
 	"net/http"
-	"os"
 	"sync"
 	"time"
 
+	"github.com/larntz/status/internal/application"
 	"github.com/larntz/status/internal/checks"
+	"github.com/larntz/status/internal/data"
+	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // TODO spawn go routines for each allowed interval 60,120,300,600,900
@@ -22,53 +23,71 @@ import (
 // TODO get ttfb or some kind of request timing
 // https://stackoverflow.com/questions/48077098/getting-ttfb-time-to-first-byte-value-in-golang/48077762#48077762
 
-func getChecks() checks.Checks {
-	// get checks
-	var checks checks.Checks
-	// contoller url sould be configurable via env
-	resp, err := http.Get("http://localhost:4242")
+func check(wg *sync.WaitGroup, dbClient *mongo.Client, checkID string, runID int64, region string, url string) {
+	defer wg.Done()
+	metadata := checks.StatusCheckMetadata{
+		Region:  region,
+		CheckID: checkID,
+	}
+	result := checks.StatusCheckResult{
+		ID:        checkID,
+		Timestamp: time.Unix(runID, 0),
+		Region:    region,
+		Metadata:  metadata,
+	}
+
+	client := http.Client{
+		Timeout: 20 * time.Second,
+	}
+	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Printf("Error getting checks from controller: %s\n", err)
+		//log.Errorf("Check result runID: %d, error: %s", runID, err)
+		result.ResponseInfo = err.Error()
+		sendCheckResult(dbClient, &result)
+		log.Errorf("%+v", result)
+		return
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("error reading body from resp: %s\n", err)
-	}
-	err = json.Unmarshal(body, &checks)
-	if err != nil {
-		fmt.Printf("error unmarshalling response from controller: %s\n", err)
-	}
-	return checks
 
+	result.ResponseCode = resp.StatusCode
+	result.ResponseInfo = resp.Status
+	result.ResponseTime = 5
+	sendCheckResult(dbClient, &result)
+
+	//log.Infof("Check result runID: %d, url: %s, status_code: %d", runID, url, resp.StatusCode)
+	log.Infof("%+v", result)
 }
 
-func check(wg *sync.WaitGroup, url string) {
-	defer wg.Done()
-	resp, err := http.Get(url)
+func sendCheckResult(dbClient *mongo.Client, result *checks.StatusCheckResult) {
+	coll := dbClient.Database("status").Collection("check_results")
+	r, err := coll.InsertOne(context.TODO(), result)
 	if err != nil {
-		fmt.Printf("http.Get error: %s\n", err)
-		os.Exit(1)
+		log.Errorf("Failed to insert document: %s", err)
 	}
-	defer resp.Body.Close()
-
-	time := time.Now().UTC()
-	fmt.Printf("CHECK: %s,%d,%s\n", url, resp.StatusCode, time)
+	log.Infof("Inserted document with _id: %v\n", r.InsertedID)
 }
 
 // StartWorker starts the worker process
-func StartWorker() {
-	checks := getChecks()
+func StartWorker(app *application.State) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(10*time.Second))
+	defer cancel()
+	data.CreateResultCollection(ctx, app.DbClient, "check_results")
+
+	checks := data.GetChecks(app.DbClient, app.Region)
 	for {
-		fmt.Println("INFO: Starting checks")
+		runID := time.Now().Unix()
+		log.Infof("Starting checks region: %s, runID: %d, %d StatusChecks, %d SSLChecks", app.Region, runID, len(checks.StatusChecks), len(checks.SSLChecks))
 		wg := new(sync.WaitGroup)
 		for _, statusCheck := range checks.StatusChecks {
 			wg.Add(1)
-			go check(wg, statusCheck.URL)
+			go check(wg, app.DbClient, statusCheck.ID, runID, app.Region, statusCheck.URL)
 		}
 		wg.Wait()
-		fmt.Printf("INFO: Completed checks\n\n")
-		time.Sleep(time.Second * 20)
-		checks = getChecks()
+		log.Infof("Completed checks region: %s, runID: %d, %d StatusChecks, %d SSLChecks", app.Region, runID, len(checks.StatusChecks), len(checks.SSLChecks))
+		time.Sleep(time.Second * 300)
+
+		app.ChecksMutex.Lock()
+		checks = data.GetChecks(app.DbClient, app.Region)
+		app.ChecksMutex.Unlock()
 	}
 }
