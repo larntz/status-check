f1755d1572b2dce0b347f520d0225d7698ab880e
Luke Arntz <luke@blue42.net>
2023-02-20 12:47:04 -0500

    add create-dev-checks subcommand

--- cmd/main.go
+++ cmd/main.go
@@ -2,24 +2,60 @@
 package main
 
 import (
-	"log"
+	"context"
+	"os"
 	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/larntz/status/cmd/controller"
 	"github.com/larntz/status/cmd/worker"
+	"github.com/larntz/status/internal/application"
 	"github.com/larntz/status/internal/data"
 )
 
-func main() {
-	log.Println("INFO: DB Connection check start")
-	if data.Ping() {
-		log.Println("INFO: DB Connection check succesful")
-	} else {
-		log.Fatal("ERROR: DB Connection check failed ")
+func dbLogin(ctx context.Context, app *application.State) {
+	log.Info("DB Login start")
+	err := data.Connect(ctx, app)
+	if err != nil {
+		log.Fatalf("DB Login failed: %s", err)
 	}
-	data.CreateDevChecks()
+	log.Info("DB Login succesful")
+}
 
-	go controller.StartController()
-	time.Sleep(time.Second * 2)
-	worker.StartWorker()
+func main() {
+	log.Info("Starting up...")
+	if len(os.Args) < 2 {
+		log.Fatal("Must specify subcommand: 'controller' or 'worker'")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var app application.State
+
+	switch os.Args[1] {
+	case "create-dev-checks":
+		if len(os.Args) < 3 {
+			log.Fatalf("Not enough args, must specify a csv file for creating checks.")
+		}
+		dbLogin(ctx, &app)
+		defer app.DbClient.Disconnect(ctx)
+		data.CreateDevChecks(app.DbClient, os.Args[2])
+	case "controller":
+		dbLogin(ctx, &app)
+		defer app.DbClient.Disconnect(ctx)
+		controller.StartController(&app)
+	case "worker":
+		region, ok := os.LookupEnv("WORKER_REGION")
+		if !ok {
+			log.Fatalf("WORKER_REGION env var not set. Exiting.")
+		}
+		app.Region = region
+		dbLogin(ctx, &app)
+		defer app.DbClient.Disconnect(ctx)
+		worker.StartWorker(&app)
+	default:
+		log.Fatal("Must specify subcommand: 'controller' or 'worker'")
+	}
 }
