38e84868b0ab4414376afe1fc4b7e7b81dc62ae4
Luke Arntz <luke@blue42.net>
2023-02-20 12:43:15 -0500

    add aplication state struct

---
+++ internal/application/application.go
@@ -0,0 +1,22 @@
+// Package application package stores our app state
+package application
+
+import (
+	"context"
+	"sync"
+	"time"
+
+	"github.com/larntz/status/internal/checks"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+// State type holds our application state
+type State struct {
+	Checks          checks.Checks
+	ChecksMutex     sync.Mutex
+	ChecksTimestamp time.Time
+	Ctx             context.Context
+	DbClient        *mongo.Client // is this abstracted enough?
+	DBClientMutex   sync.Mutex
+	Region          string
+}
