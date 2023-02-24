424ce036812c21abe6061828771d9e04a9ec02a3
Luke Arntz <luke@blue42.net>
2023-02-24 18:40:48 -0500

    check(): move allocations out of for{}.

--- cmd/worker/worker.go
+++ cmd/worker/worker.go
@@ -5,84 +5,18 @@ import (
 	"context"
 	"math/rand"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/larntz/status/internal/application"
 	"github.com/larntz/status/internal/checks"
 	"github.com/larntz/status/internal/data"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
 )
-
-// TODO spawn go routines for each allowed interval 60,120,300,600,900
-// and assign checks to threads
-// OR
-// spawn a goroutine to handle groups of checks with the same interval
-// 100 checks x60s could be split and assigned to 5 goroutines with 20 checks each
 
 // TODO get ttfb or some kind of request timing
 // https://stackoverflow.com/questions/48077098/getting-ttfb-time-to-first-byte-value-in-golang/48077762#48077762
-
-func check(ctx context.Context, delay int, app *application.State, check *checks.StatusCheck) {
-	time.Sleep(time.Duration(delay) * time.Second)
-
-	metadata := checks.StatusCheckMetadata{
-		Region:  app.Region,
-		CheckID: check.ID,
-	}
-	for {
-		result := checks.StatusCheckResult{
-			ID:        check.ID,
-			Timestamp: time.Now().UTC(),
-			Region:    app.Region,
-			Metadata:  metadata,
-		}
-
-		client := http.Client{
-			Timeout: 15 * time.Second,
-		}
-		resp, err := client.Get(check.URL)
-		if err != nil {
-			result.ResponseInfo = err.Error()
-			go sendCheckResult(app, &result)
-			log.Errorf("%+v", result)
-			return
-		}
-		defer resp.Body.Close()
-
-		result.ResponseCode = resp.StatusCode
-		result.ResponseInfo = resp.Status
-
-		result.ResponseTime = 5
-		go sendCheckResult(app, &result)
-
-		log.Infof("%+v", result)
-		time.Sleep(time.Duration(check.Interval) * time.Second)
-	}
-}
-
-func startChecks(runID int64, app *application.State, workerChecks *checks.Checks) {
-	log.Infof("Starting checks region: %s, runID: %d, %d StatusChecks, %d SSLChecks", app.Region, runID, len(workerChecks.StatusChecks), len(workerChecks.SSLChecks))
-	app.ChecksMutex.Lock()
-
-	rand.Seed(time.Now().Unix())
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-	for _, statusCheck := range workerChecks.StatusChecks {
-		delay := rand.Int() % statusCheck.Interval
-		go check(ctx, delay, app, &statusCheck)
-	}
-	app.ChecksMutex.Unlock()
-}
-
-func sendCheckResult(app *application.State, result *checks.StatusCheckResult) {
-	coll := app.DbClient.Database("status").Collection("check_results")
-	iResult, err := coll.InsertOne(context.TODO(), result)
-	if err != nil {
-		log.Errorf("Failed to InsertMany: %s", err)
-		return
-	}
-	log.Infof("Successfully inserted document %s, checkID: %s", iResult.InsertedID, result.ID)
-}
 
 // StartWorker starts the worker process
 func StartWorker(app *application.State) {
@@ -91,14 +25,67 @@ func StartWorker(app *application.State) {
 	data.CreateResultCollection(ctx, app.DbClient, "check_results")
 
 	workerChecks := data.GetChecks(app.DbClient, app.Region)
-	for {
-		runID := time.Now().Unix()
-		log.Infof("running %d checks", len(workerChecks.StatusChecks))
-		go startChecks(runID, app, &workerChecks)
+	var wg sync.WaitGroup
+	app.ChecksMutex.Lock()
+	rand.Seed(time.Now().Unix())
+	for _, statusCheck := range workerChecks.StatusChecks {
+		delay := rand.Int() % statusCheck.Interval
+		wg.Add(1)
+		go check(&wg, delay, app, statusCheck)
 
-		time.Sleep(time.Second * 300)
-		app.ChecksMutex.Lock()
-		workerChecks = data.GetChecks(app.DbClient, app.Region)
-		app.ChecksMutex.Unlock()
+		// TODO check for new checks and create new goroutines for new checks
 	}
+	app.ChecksMutex.Unlock()
+	wg.Wait()
+}
+
+func check(wg *sync.WaitGroup, delay int, app *application.State, check checks.StatusCheck) {
+	defer wg.Done()
+	log.Infof("Preparing check %+v", check)
+	time.Sleep(time.Duration(delay) * time.Second)
+
+	result := checks.StatusCheckResult{
+		Metadata: checks.StatusCheckMetadata{
+			Region:  app.Region,
+			CheckID: check.ID,
+		},
+	}
+
+	client := http.Client{
+		Timeout: 15 * time.Second,
+	}
+
+	for {
+		result.Timestamp = time.Now().UTC()
+		resp, err := client.Get(check.URL)
+		if err != nil {
+			result.ResponseInfo = err.Error()
+			go sendCheckResult(app.DbClient, &result)
+			log.Errorf("%+v", result)
+			return
+		}
+
+		result.ResponseCode = resp.StatusCode
+		result.ResponseInfo = resp.Status
+
+		// done with resp
+		resp.Body.Close()
+
+		result.ResponseTime = 5
+		go sendCheckResult(app.DbClient, &result)
+
+		log.Infof("Check Result: %+v", result)
+		log.Infof("Check %s sleeping for %d seconds", check.ID, check.Interval)
+		time.Sleep(time.Duration(check.Interval) * time.Second)
+	}
+}
+
+func sendCheckResult(client *mongo.Client, result *checks.StatusCheckResult) {
+	coll := client.Database("status").Collection("check_results")
+	iResult, err := coll.InsertOne(context.TODO(), result)
+	if err != nil {
+		log.Errorf("Failed to InsertMany: %s", err)
+		return
+	}
+	log.Infof("Successfully inserted document %s, checkID: %s", iResult.InsertedID, result.Metadata.CheckID)
 }
